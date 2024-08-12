4b78bc728ac92b252ed3fb5fd76ff6a3fe7f7681
花房健太郎 <152841089+hanafusa0108@users.noreply.github.com>
2024-08-13 00:35:04 +0900

    Add files via upload

---
+++ 004.py
@@ -0,0 +1,19 @@
+#入力の読み込み
+H, W = map(int,input().split()) 
+A = [list(map(int, input().split())) for l in range(H)]
+
+#前処理
+
+#横の和
+width = list(map(sum, A))
+
+#縦の和
+length = list(map(sum, zip(*A)))
+
+#結果を出力
+for i in range(H):
+    result = []
+    for j in range(W):
+        result.append(width[i] + length[j] - A[i][j])
+    
+    print(*result)
