ac2d6650cfa446d5f73fa158c26cb58c5a503654
花房健太郎 <152841089+hanafusa0108@users.noreply.github.com>
2024-08-27 11:05:27 +0900

    Add files via upload

---
+++ 061-Deck.py
@@ -0,0 +1,16 @@
+# 入力
+Q = int(input())
+tx = [map(int, input().split()) for _ in range(Q)]
+t, x = [list(i) for i in zip(*tx)]
+
+# 出力
+card = []
+for i in range(Q):
+    if t[i] == 1:
+        card.insert(0, x[i])
+    
+    elif t[i] == 2:
+        card.append(x[i])
+
+    else:
+        print(card[x[i]-1])
