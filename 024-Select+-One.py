d17efcefeeb2f91edc04fc0865c8c38cc7afd18e
花房健太郎 <152841089+hanafusa0108@users.noreply.github.com>
2024-08-27 11:46:35 +0900

    Add files via upload

---
+++ 024-Select+-One.py
@@ -0,0 +1,28 @@
+import sys
+
+def main():
+    # 入力
+    N, K = map(int, input().split())
+    A = list(map(int, input().split()))
+    B = list(map(int, input().split()))
+
+    # 増減の回数
+    diff = 0
+    for i in range(N):
+        diff += abs(B[i] - A[i])
+
+    counter = K - diff
+
+    # 出力
+    if counter < 0:
+        print("No")
+        sys.exit()
+
+    if counter % 2 != 0:
+        print("No")
+        sys.exit()
+
+    print("Yes")
+
+if __name__ == '__main__':
+    main() 
